4f26956899f7042e86f24c7c7266f3e0345eadfc
Blackedhs <fedepcs@gmail.com>
2020-08-27 22:27:08 -0300

    segunda vez

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'labo4';
+  public edadUno = '';
+  public edadDos = '';
+  public suma;
+  public promedio;
+  CalcularClick(){
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma /2 ;
+    document.getElementById('pPromedio').style.display = 'block';
+    document.getElementById('pSuma').style.display = 'block';
+  }
+  LimpiarClick(){
+    this.edadUno ='';
+    this.edadDos ='';
+    document.getElementById('pPromedio').style.display = 'none';
+    document.getElementById('pSuma').style.display = 'none';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ErrorComponent } from './componentes/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
