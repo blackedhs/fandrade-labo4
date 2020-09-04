0f4f9ed75507897074e7c68cd7265361b31fcd52
Blackedhs <fedepcs@gmail.com>
2020-09-03 22:18:20 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,9 @@ import { BienvenidosComponent } from './componentes/bienvenidos/bienvenidos.comp
 import { CalculoComponent } from './componentes/calculo/calculo.component';
 const routes: Routes = [
   {path : 'login' , component : LoginComponent},
-  {path : '' , component : CalculoComponent},
+  {path : '' , component : LoginComponent},
   {path : 'error' , component : ErrorComponent},
+  {path : 'calculo' , component : CalculoComponent},
   {path : 'bienvenido' , component : BienvenidosComponent},
   {path : '*' , pathMatch : 'full' , redirectTo : 'bienvenido' }
 ];
